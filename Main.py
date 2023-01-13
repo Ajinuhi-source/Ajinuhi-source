6b7f73b52d8ab532475a39d968efc144ccff6061
Ajinuhi Oluwabunmi Faith <120075481+Ajinuhi-source@users.noreply.github.com>
2023-01-13 15:16:25 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,9 +24,6 @@ def get_channel_stats(youtube, channel_ids):
                     playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads'] )
     return data
 
-# Let's check a random channel to see how it works: 
-
-get_channel_stats(youtube, channel_ids[0])
 
 # Let's put all general statistics in one dataframe.
 
@@ -37,14 +34,13 @@ for i in range(len(channel_ids)):
     general_stats = pd.concat([general_stats, data], axis=0)
 
 general_stats = general_stats.reset_index(drop=True)
-general_stats
+
 
 # function to get all video ids of a channel.
 
 def get_video_ids(youtube, playlist_id):
     request = youtube.playlistItems().list(part = 'contentDetails',playlistId = playlist_id, maxResults=100)
     response = request.execute()
-    
     videos_id = []
     for i in range(len(response['items'])):
         videos_id.append(response['items'][i]['contentDetails']['videoId'])
@@ -74,6 +70,7 @@ video_ids = []
 for i in range(len(channel_ids)):
     video_ids.append(get_video_ids(youtube, general_stats.playlist_id.iloc[i]))
 
+    
 # function to get video details
 
 def get_video_details(youtube, video_ids):
@@ -92,11 +89,12 @@ def get_video_details(youtube, video_ids):
             
     return all_video_stats
 
-# Let's check how the function works.
 
+# Let's check how the function works.
 video_stats = get_video_details(youtube, video_ids[8])
 video_stats = pd.DataFrame(video_stats)
 video_stats.head()
+
 
 # Storing all video information dataframes in a list.
 
@@ -107,7 +105,7 @@ for i in range(len(channel_ids)):
     df['Identity'] = general_stats.Channel_name.iloc[i]  # to know to which channel the video belongs.
     video_dfs.append(df)
 
-video_dfs[-1].head()
+    
 
 # Concating all video_dfs dataframes into one big dataframe.
 
@@ -118,19 +116,6 @@ for df in video_dfs:
 
 video_df= video_df.reset_index(drop=True)
 
-video_df.shape
-
-### Data Cleaning
-
- #Let's take a look on Channel stats
-general_stats.head()
-
-# check for missing values
-
-general_stats.isnull().sum()
-
-# Let's check the type of the columns.
-general_stats.dtypes
 
 # Let's change the type of the numeric columns to int, and the date column to date.
 general_stats['Subscribers_Count'] = pd.to_numeric(general_stats['Subscribers_Count'])
@@ -138,15 +123,6 @@ general_stats['Total_Views_Count'] = pd.to_numeric(general_stats['Total_Views_Co
 general_stats['Total_Videos'] = pd.to_numeric(general_stats['Total_Videos'])
 general_stats['DateStarted'] = pd.to_datetime(general_stats['DateStarted']).dt.date.astype('datetime64')
 
-general_stats.dtypes
-
-# There is no need to check the video_id list since its just strings
-
-video_df.head()
-
-video_df.isnull().sum()
-
-video_df.dtypes
 
 # Let's change the dtypes
 video_df['Views'] = pd.to_numeric(video_df['Views'])
@@ -154,134 +130,127 @@ video_df['Likes'] = pd.to_numeric(video_df['Likes'])
 video_df['Comments'] = pd.to_numeric(video_df['Comments'])
 video_df['Publish_Date'] = pd.to_datetime(video_df['Publish_Date']).dt.date.astype('datetime64')
 
-video_df.dtypes
 
-video_df.head(10)
+###############################################################################################
+##################################### Support Functions #######################################
+###############################################################################################
 
-### Exploratory Data Analysis
 
-# Starting with general_stats
 
-general_stats.describe()
+# Function to return top 10 viewed videos ever.
 
-general_stats.shape
+def top_10_viewed(df):
+    fig, ax = plt.subplots()
+    plt.ticklabel_format(style='plain', axis='y')
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000000) + 'M'))
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    ordered = df.sort_values('Views', ascending=False)
+    top_10 = ordered.head(10)
+    plt.xticks(rotation=90)
+    sns.barplot(y=top_10['Views'] , x=top_10['Title'], palette ='Blues_r').set_xticklabels(labels = top_10['Title'],fontproperties=fprop, fontsize=12);
+    for bar, label in zip(ax.patches, top_10['Identity']):
+        x = bar.get_x()
+        width = bar.get_width()
+        height = bar.get_height()
+        ax.text(x+width/2., height + 0.2, label, ha="center", fontproperties=fprop, fontsize=3.5) 
 
-# We have 10 data science channels to analyse with 6 different attributes
 
-# how many videos we have in total?
 
-general_stats['Total_Videos'].sum()
+# Function to return top 10 viewed videos for a specific channel.
 
-# Checking the distribution of the number of subscribers.
+def top_10_viewed_by_name(df, name):
+    fig, ax = plt.subplots()
+    ax.ticklabel_format(useOffset=False, style='plain')
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000000) + 'M'))
 
-sns.displot(general_stats, x='Subscribers_Count')
-plt.title('Subscribers distribution')
-
-# Most of the channels have 0-1 million subscribers
-
-# let's see how many subscribers each channel have.
-
-plt.figure(figsize = (10,8))
-names = general_stats['Channel_name']
-subs = general_stats['Subscribers_Count']
-names_ordered = general_stats.sort_values('Subscribers_Count', ascending=False).Channel_name
-plt.title('Number of Channel Subscribers')
-fig = sns.barplot(x=subs, y=names, palette='Blues_r', order=names_ordered).set_yticklabels(labels=names_ordered,fontsize=12)
-fig;
-
-# Sort by date started to check oldest vs newest channel.
-
-plt.figure(figsize = (10,8))
-plt.xticks(rotation=90)
-plt.title('Oldest VS Newest Channel')
-fig = sns.lineplot(x=general_stats['Channel_name'], y=general_stats['DateStarted'], palette='Blues').set_xticklabels(labels = names)
-fig;
-
-# Number of videos plot
-
-plt.xticks(rotation=90)
-plt.title('Number of Videos for Channels')
-names = general_stats['Channel_name']
-vids = general_stats['Total_Videos']
-
-names_ordered = general_stats.sort_values('Total_Videos', ascending=False).Channel_name
-
-fig = sns.barplot(y=vids, x=names, palette ='Blues_r', order=names_ordered).set_xticklabels(labels = names_ordered, fontsize=12)
-fig ;
-
-# Exploratory Data Analysis on Video_df
-
-video_df.head()
-
-video_df.describe()
-
-# Function to return top 10 viewed videos for AleX The Analyst channel.
-def top_5_viewed_by_name(df, name):
     name_df = df[df.Identity == name]
     ordered = name_df.sort_values('Views', ascending=False)
-    top_5 = ordered.head(5)
+    top_10 = ordered.head(10)
     plt.xticks(rotation=90)
-    plt.title("Top Viewed Videos of Alex The Analyst")
-    plot = sns.barplot(y=top_5['Views'] , x=top_5['Title'], palette ='Blues_r').set_xticklabels(labels = top_5['Title'], fontsize=12)
-    plot;
+    sns.barplot(y=top_10['Views'] , x=top_10['Title'], palette ='Blues_r').set_xticklabels(labels = top_10['Title'],fontproperties=fprop, fontsize=12);
 
-top_5_viewed_by_name(video_df, 'Alex The Analyst')
 
-#  top 10 viewed videos for all the channels.
-top_10_viewed = video_df.sort_values(by="Views",ascending=False).head(10)
-top_10_viewed
-
-Title = top_10_viewed["Title"]
-Views = top_10_viewed["Views"]
-plt.bar(Title, Views)
-plt.title("Top viewed videos")
-plt.xlabel("Views")
-plt.ylabel("Title")
-plt.xticks(rotation=90)
-plt.show()
-
-# Most Liked videos ever.
+# TOP 10 Liked videos ever.
 
 def top_10_Liked(df):
+    fig, ax = plt.subplots()
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K'))
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
     ordered = df.sort_values('Likes', ascending=False)
     top_10 = ordered.head(10)
     plt.xticks(rotation=90)
-    plt.title("Top Liked videos")
-    plot = sns.barplot(y=top_10['Likes'] , x=top_10['Title'], palette ='Blues_r').set_xticklabels(labels = top_10['Title'], fontsize=12)
-    plot;
+    sns.barplot(y=top_10['Likes'] , x=top_10['Title'], palette ='Blues_r').set_xticklabels(labels = top_10['Title'],fontproperties=fprop, fontsize=12);
+    for bar, label in zip(ax.patches, top_10['Identity']):
+        x = bar.get_x()
+        width = bar.get_width()
+        height = bar.get_height()
+        ax.text(x+width/2., height + 0.2, label, ha="center", fontproperties=fprop, fontsize=3.5) 
 
-top_10_Liked(video_df)
 
-# Top 10 Liked Videos by Channel
+
+# Top 10 Liked Videos by Channel.
 
 def top_10_Liked_by_name(df, name):
+    fig, ax = plt.subplots()
+    ax.ticklabel_format(useOffset=False, style='plain')
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K'))
     name_df = df[df.Identity == name]
     ordered = name_df.sort_values('Likes', ascending=False)
     top_10 = ordered.head(10)
     plt.xticks(rotation=90)
-    plt.title("Top Liked Videos of Corey Schafer")
-    plot = sns.barplot(y=top_10['Likes'] , x=top_10['Title'], palette ='Blues_r').set_xticklabels(labels = top_10['Title'], fontsize=12)
-    plot;
+    sns.barplot(y=top_10['Likes'] , x=top_10['Title'], palette ='Blues_r').set_xticklabels(labels = top_10['Title'],fontproperties=fprop, fontsize=12);
 
 
-top_10_Liked_by_name(video_df, 'Corey Schafer')
+# Count of subscribers for each channel.
+
+def sub_count(df):
+    fig, ax = plt.subplots()
+    ax.ticklabel_format(useOffset=False, style='plain')
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.get_xaxis().set_visible(False)
+    names = general_stats['Channel_name']
+    subs = general_stats['Subscribers_Count']
+    names_ordered = general_stats.sort_values('Subscribers_Count', ascending=False).Channel_name
+    sns.barplot(x=subs, y=names, palette='Blues_r', order=names_ordered).set_yticklabels(labels=names_ordered,fontproperties=fprop,fontsize=12);
+    ax.bar_label(ax.containers[0], fmt = '%d');
 
 
-# Lets save the general stats and video_df so we can build dashboard
-general_stats.to_csv('general.csv')
-video_df.to_csv('videos.csv')
+# Line plot of dates the channels got started.
 
-### Creating a Web App with Streamlit
+def timeline(df):
+    fig, ax = plt.subplots();
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    plt.xticks(rotation=90);
+
+    names = general_stats['Channel_name']
+    ordered = general_stats.sort_values('DateStarted', ascending=True)
+
+    sns.lineplot(x=ordered['Channel_name'], y=ordered['DateStarted'], palette='Blues', marker="o").set_xticklabels(labels = ordered['Channel_name'],fontproperties=fprop, fontsize=12);
+
+    
+    
+
+###############################################################################################
+##################################### STREAMLIT PART ##########################################
+###############################################################################################
+
 
 import streamlit as st
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(layout="wide")
 
+
 sidebar_list = general_stats.Channel_name.tolist()
 sidebar_list.insert(0, "General Stats")
 
 side_bar = st.sidebar.selectbox('Which channel would you like to check ?', sidebar_list)
+
 
 if side_bar == "General Stats":
 
@@ -330,6 +299,8 @@ if side_bar == "General Stats":
     st.write(' #### Top 10 ***Liked***  Videos : ')
     st.pyplot(top_10_Liked(video_df))
 
+
+
 def st_page(name):
 
     st.write(f""" # {name} Channel Analysis """)
@@ -364,8 +335,7 @@ def st_page(name):
     st.write(' ### Top 10 liked videos for this channel: ')
     st.pyplot(top_10_Liked_by_name(video_df, name))
 
-if side_bar == '':
-    st_page('')
+
 
 if side_bar == 'techTFQ':
     st_page('techTFQ')
